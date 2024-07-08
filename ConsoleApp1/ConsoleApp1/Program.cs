daa126250d150cb7f9178de37297dfdd089a4e40
John Fox <john.fox@capgemini.com>
2024-07-08 12:26:02 +0100

    My first comit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string dbCS = "Server=tcp:devtrsdbssq1001.database.windows.net,1433;Initial Catalog=trade-application-status;Persist Security Info=False;User ID=sqladmuser;Password=<E3>BWzNKpOX;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
